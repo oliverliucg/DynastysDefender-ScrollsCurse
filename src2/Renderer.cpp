66a3add4b69fdae3a54233f93cd1519a6c35641e
Oliver Liu <oliver.liu.cg@outlook.com>
2024-04-23 08:46:04 -0700

    rename src2 to src

--- src2/Renderer.cpp
+++
@@ -1,8 +0,0 @@
-#include "Renderer.h"
-
-Renderer::Renderer(const Shader& shader) : shader(shader) {}
-
-Renderer::~Renderer() {
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-}
